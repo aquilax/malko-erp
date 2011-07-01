f642749f96a5eab8fff34f375e43e146f4899b6d
Evgeni Vasilev <aquilax@gmail.com>
2011-07-01 16:42:21 +0300

    Added product list;

--- app_be/classes/model/product.php
+++ app_be/classes/model/product.php
@@ -6,7 +6,23 @@ class Model_Product extends Model {
     return DB::select('*')
       ->from('product')
       ->where('id', $id)
-      ->execute();
+      ->where('status_id', '>', 0)
+      ->execute()
+      ->as_array();
+  }
+
+  function getList($config){
+    $res = array();
+    $res['data']  = DB::select('*')
+      ->from('product')
+      ->where('status_id', '>', 0)
+      ->order_by($config['sort'])
+      ->limit($config['limit'])
+      ->offset($config['start'])
+      ->execute()
+      ->as_array();
+    $res['totalCount'] = DB::count_records('product');
+    return $res;
   }
 
 }
